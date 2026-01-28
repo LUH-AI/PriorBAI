f98b89562417803c26a16f49b8897bd4bdc8adac
Lukas Fehring <lukasfehring@gmail.com>
2026-01-28 15:58:28 +0100

    Anonymize

--- priorbai/__init__.py
+++ priorbai/__init__.py
@@ -1,7 +1,7 @@
 """Top-level package for PriorBAI."""
 import datetime
 
-__author__ = """AutoML Hannover"""
-__email__ = """m.wever@ai.uni-hannover.de"""
+__author__ = """Orgname"""
+__email__ = """name@email"""
 __version__ = """0.1.0"""
-__copyright__ = f"Copyright {datetime.date.today().strftime('%Y')}, AutoML Hannover"
+__copyright__ = f"Copyright {datetime.date.today().strftime('%Y')}, Orgname"
--- run_experimenter.sh
+++ run_experimenter.sh
@@ -5,12 +5,12 @@
 #SBATCH --mem-per-cpu 4G
 #SBATCH -J priorbai
 #SBATCH -p normal
-#SBATCH --mail-user l.fehringr@ai.uni-hannover.de
+#SBATCH --mail-user l.namer@email
 #SBATCH --mail-type ALL
 #SBATCH -o logs/priorbai_%A_%a.out
 #SBATCH -e logs/priorbai_%A_%a.err
 
-cd /scratch/hpc-prf-intexml/fehring/PriorBAI/
+cd /scratch/name/name/PriorBAI/
 
 source .venv/bin/activate
 
